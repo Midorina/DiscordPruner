af56e12d412e3b6e7367446a52f8c1e9fffde4b6
Midoriin <fbyigit07@hotmail.com>
2020-01-07 17:46:05 +0300

    For some reason this file was pushed as an empty file.

---
+++ main.py
@@ -0,0 +1,37 @@
+from discord.ext import commands
+import asyncio
+import logging
+import json
+
+with open('config.json') as f:
+    config = json.load(f)
+
+bot = commands.Bot(command_prefix=config['prefix'])
+
+logging.basicConfig(level=logging.INFO)
+
+
+@bot.command()
+async def prune(ctx):
+    if ctx.author.id == config['owner_id']:
+        members = ctx.guild.members
+
+        for member in members:
+            if not member.bot and member.id not in config['whitelisted_member_ids']:
+                msg = await ctx.send(f"Kicking {str(member)}...")
+                counter = 0
+                while counter < 3:
+                    try:
+                        await member.kick(reason="Server prune.")
+                        await msg.edit(content=f"Kicking {str(member)}... **Success!**")
+                        await asyncio.sleep(1.0)
+                        break
+
+                    except Exception as e:
+                        print(f"Failed to kick {str(member)}.")
+                        print(e)
+                        print("Trying again...")
+                        counter += 1
+                        continue
+
+bot.run(config['bot_token'])
